b5c11a6894ff99a7ade8362f0e5894740959e71d
LnickOne <manki0zz@126.com>
2025-11-22 01:10:57 +0800

    update

--- Python3/LeetCode/11.盛最多水的容器.py
+++ Python3/LeetCode/11.盛最多水的容器.py
@@ -9,18 +9,17 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_area = 0
+        result = 0
         left = 0
         right = len(height) - 1
         while left < right:
             length = right - left
             area = length * min(height[left], height[right])
-            max_area = max(max_area, area)
+            max_area = max(result, area)
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-        return max_area
-
+        return result
 
 # @lc code=end
