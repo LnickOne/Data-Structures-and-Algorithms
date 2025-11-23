04d4b16ef4171a87226638bee0f51a75368e34eb
LnickOne <manki0zz@126.com>
2025-11-23 20:38:57 +0800

    update

---
+++ Python3/LeetCode/0012.整数转罗马数字.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=12 lang=python3
+#
+# [12] 整数转罗马数字
+#
+
+
+# @lc code=start
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        result = ""
+        symbols = [
+            "M",
+            "CM",
+            "D",
+            "CD",
+            "C",
+            "XC",
+            "L",
+            "XL",
+            "X",
+            "IX",
+            "V",
+            "IV",
+            "I",
+        ]
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        for i in range(len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                result += symbols[i]
+        return result
+
+
+print(Solution().intToRoman(3))
+print(Solution().intToRoman(4))
+print(Solution().intToRoman(9))
+print(Solution().intToRoman(58))
+print(Solution().intToRoman(1994))
+
+
+# @lc code=end
